4c8e18390229b7a06d854fa72a9c5d523c9a08cd
たけ <takehphp525@gmail.com>
2015-12-20 10:23:00 +0900

    js
    tweetのtextureにfocusするとtextarea拡張アンドついーとボタンがdisplay:block;

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,19 @@
-﻿
+$(function(){
+//
+//  $('textarea').on('focus').function(){
+//    alert('aaa')
+//    $(this).attr('rows'.'10');
+//  }).on('blur',function(){
+//    $(this).sttr('attr','3');
+//  })
+
+ $("textarea.tweet_textarea").focus(function(){
+              $(this).css('height','150px');
+              $("div.on_focus").css('display','block');
+           }).blur(function(){
+               $(this).css('height','58px');
+               $("div.on_focus").css('display','none');
+           });
+
+
+});
